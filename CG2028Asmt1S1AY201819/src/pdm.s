5c4c63a9c512103f25e02bd569630b8716cd7c2d
Pham Quang Minh <px1099@gmail.com>
2018-09-09 21:03:41 +0800

    Add Machine Code Comments

--- CG2028Asmt1S1AY201819/src/pdm.s
+++ CG2028Asmt1S1AY201819/src/pdm.s
@@ -13,24 +13,24 @@ pdm:
 	@ using a stack to store register states
 	PUSH {R3, R4}
 
-	@ Loading nmm into R3
+	@ Loading n[m,m] into R3
 	LSL R2, R2, #2
-	MLA R0, R1, R2, R0
-	ADD R3, R0, R2
-	LDR R3, [R3]
+	MLA R0, R1, R2, R0	@ 0x00200211
+	ADD R3, R0, R2		@ 0x00803002
+	LDR R3, [R3]		@ 0x04933000
 
 	@ Calculate sum of row
-	MOV R4, #0
+	MOV R4, #0		@ 0x03A44000
 
 loop:
-	LDR R2, [R0], #4
-	ADD R4, R2
-	SUBS R1, #1
-	BNE loop
+	LDR R2, [R0], #4	@ 0x04902000
+	ADD R4, R2		@ 0x00844002
+	SUBS R1, #1		@ 0x02511001
+	BNE loop		@ 0x18000010
 
 	@ Calculate PDm
 	MOVW R1, SCALING
-	MUL R3, R1
+	MUL R3, R1		@ 0x00003113
 	SDIV R0, R3, R4
 
 	@ Restoring Stack
